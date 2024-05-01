132041d3b944b5b05047624db8eb795c0f0a761e
Tim <timothyszandi@gmail.com>
2024-05-01 19:43:59 +0800

    Added the voting functionality that persists within the session, not to the database model yet. This is printed to terminal now for reference.

--- app.py
+++ app.py
@@ -12,13 +12,40 @@ def get_image_filenames():
 
 @app.route('/')
 def home():
-    # Example image data (replace with your actual image data)
     image_filenames = get_image_filenames()
+    
+    for filename in image_filenames:
+        """ THIS NEEDS TO BE RETRIEVED DYNAMICALLY FROM DATABASE NOT JUST SET TO 0 """
 
     # Retrieve poll_data from session, or initialize with zeros if it doesn't exist
     poll_data = session.get('poll_data')
+    print(poll_data)
     if poll_data is None:
         poll_data = {image_name: {'yes': 0, 'no': 0} for image_name in image_filenames}
         session['poll_data'] = poll_data
+        print(poll_data)
     
     return render_template('index.html', images=image_filenames, poll_data=poll_data)
+
+@app.route('/vote', methods=['GET', 'POST'])
+def vote():
+    if request.method == 'POST':
+        #Do not want the whole extended path
+        selected_candidate = request.form['image'].split("/")[-1]
+        print(selected_candidate)
+        yesVote = request.form['choice'] == "yes"
+        print(request.form['choice'])
+        votes = session.get('poll_data')
+        if yesVote:
+            votes[selected_candidate]["yes"] += 1
+        else:
+            votes[selected_candidate]["no"] += 1
+
+        session['poll_data'] = votes
+
+    return home()
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
